118a2e84466eccf827c9144a21601c498eadf9b3
Arijit Paria <arijitparia2020@gmail.com>
2024-01-29 15:12:17 +0530

    add billing section

--- src/components/Billing.tsx
+++ src/components/Billing.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 
 const Billing = () => {
   return (
-    <div>
-      Billing
-    </div>
-  )
-}
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img
+          src={bill}
+          alt="billing"
+          className="h-[100%] w-[100%] relative z-[5]"
+        />
+        <div className="absolute z-[3] -left-1/2 top-0 w-1/2 h-1/2 rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-1/2 h-1/2 rounded-full pink__gradient" />
+      </div>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden" /> Billing &
+          Invoicing
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit.
+        </p>
+        <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+          <img src={apple} alt="apple store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+          <img src={google} alt="google play" className="w-[128px] h-[42px] object-contain  cursor-pointer" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Billing
+export default Billing;
